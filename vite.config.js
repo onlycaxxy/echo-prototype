d4e89933fba748438875fedce741858716d4dfa2
Cccaxxy ooonly <cccaxxyooonly@CccaxxydeMacBook-Air.local>
2025-04-07 09:46:45 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   root: './',
   base: '/',
   build: {
-    outDir: 'build',
-  }
+    outDir: 'dist',
+  },
+  plugins: [vue()]
 })
 
